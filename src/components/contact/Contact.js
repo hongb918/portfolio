cbda2c3a728d44453eda1c29401af2935c8cd319
hongb918 <hong.b918@gmail.com>
2022-08-03 23:47:13 -0500

    web frame complete

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import './Contact.css'
-
+import Typewriter from 'typewriter-effect'
 
 function Contact() {
     return (
-        <section id='contact'>Contact
+        <section id='contact'>
+            <h2 className="title">Get In Touch</h2>
+            <p> I am looking for opportunities in front-end development.</p>
+            <button className='btn'>
+                <a className='email-link' href='mailto:hong.b918@gmail.com'>Say Hello</a>
+            </button>
+            <div className='typewriter'>
+
+                <Typewriter
+                    options={{
+                        strings: ['hong.b918@gmail.com'],
+                        autoStart: true,
+                        loop: true,
+                        pauseFor: 1500,
+                        delay: 100,
+                        deleteSpeed: 150,
+                    }}
+                />
+            </div>
         </section>
     )
 }
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,32 @@
 import React from 'react'
 import './footer.css'
+import { SiGithub } from 'react-icons/si'
+import { SiLinkedin } from 'react-icons/si'
 
 
 function Footer() {
     return (
-        <div>Footer</div>
+        <footer>
+            <a href="#" className='footer__logo'>
+                HONG
+            </a>
+            <ul className='permalinks'>
+                <li><a href ='#'>Home</a></li>
+                <li><a href ='#about'>About</a></li>
+                <li><a href ='#experience'>Experience</a></li>
+                <li><a href ='#portfolio'>Portfolio</a></li>
+                <li><a href ='#contact'>Contact</a></li>
+            </ul>
+
+            <div className='footer__socials'>
+                <a href='https://github.com/hongb918'><SiGithub/></a>
+                <a href='https://www.linkedin.com/in/hong-bui918/'><SiLinkedin /></a>
+            </div>
+
+            <div className='footer__copyright'>
+                <small>&copy;Hong Bui</small>
+            </div>
+        </footer>
     )
 }
 
